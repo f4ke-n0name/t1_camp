e576f97e8c02153d315290537fbd83fc5355ff89
Danil <danil.abdulminev@mail.ru>
2025-07-28 15:30:56 +0300

    feat: added docker compose

---
+++ github/src/test/java/com/f4ken0name/github/GithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.f4ken0name.github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
